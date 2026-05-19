e0e50d32ca67cc57007f4b959a650c1e873d21fc
ANDRY <andry.andry@student.passerellesnumeriques.org>
2026-05-19 17:19:17 +0300

    Ajout de la documentation dans le code

--- wallet.py
+++ wallet.py
@@ -6,7 +6,14 @@ class Wallet:
         self.balance = self.balance + val
 
     def get_balance(self):
+        """
+        Returns the balance
+
+        Returns:
+            float: The balance
+        """
         return self.balance
+    
 
     def remove_balance(self, val):
         self.balance = self.balance - val
